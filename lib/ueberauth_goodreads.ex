51d991bd2182244d1dbb5bee2419c45e0e5843f8
Tyler Clemens <tyler.r.clemens@gmail.com>
2017-02-19 16:36:16 -0800

    Inital commit

---
+++ lib/ueberauth_goodreads.ex
@@ -0,0 +1,18 @@
+defmodule UeberauthGoodreads do
+  @moduledoc """
+  Documentation for UeberauthGoodreads.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UeberauthGoodreads.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ueberauth_goodreads_test.exs
@@ -0,0 +1,8 @@
+defmodule UeberauthGoodreadsTest do
+  use ExUnit.Case
+  doctest UeberauthGoodreads
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
